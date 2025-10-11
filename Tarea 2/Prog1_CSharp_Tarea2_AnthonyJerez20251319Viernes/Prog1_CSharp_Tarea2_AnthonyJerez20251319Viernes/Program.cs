8bce1cca2b3612d2181a829bb1be36538c198321
AnthonyJG-Noob2 <anthonyjesusjerezgomez.mymail@gmail.com>
2025-10-11 14:50:48 -0400

    Uploading Second C# Homework to GitHub
    Uploading the second C# Homework about even and odd numbers. Basically checking if a number can be divided by 2 and the result in the substraction is zero (even) or if that is not the case and the number does not have any half (odd).
    
    Second Homework is on the way.

---
+++ Tarea 2/Prog1_CSharp_Tarea2_AnthonyJerez20251319Viernes/Prog1_CSharp_Tarea2_AnthonyJerez20251319Viernes/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+// Anthony Jerez (2025-1319)
+// using top level statements
+// In order to debug the code, please use .NET 9.0
+
+Console.WriteLine("Let´s check if the number is even or odd");
+
+bool active = true;
+int chosenumber = 0;
+
+    while (active == true)
+    {
+    
+        Console.WriteLine("Write the number");
+    try
+    {
+       chosenumber  = int.Parse(Console.ReadLine());
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("User, please write a number");
+        continue;
+    }
+        
+        if (chosenumber % 2 == 0)
+        {
+            Console.WriteLine($"{chosenumber} is an even number");
+        }
+        else
+    {
+        Console.WriteLine($"{chosenumber} is an odd number");
+    }
+
+        Console.WriteLine("Want to continue?");
+        Console.WriteLine("1 = yes/ 0 = no");
+        int option = int.Parse(Console.ReadLine());
+
+        if (option == 0)
+        {
+            active = false;
+        }
+        else
+        {
+            Console.WriteLine("Let´s go then");
+        }
+    } 
+
+
+    Console.WriteLine("Thank you for using this app");
+    Console.WriteLine("Press any key to exit");
+    Console.ReadKey();
